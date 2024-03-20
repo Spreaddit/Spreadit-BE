64d72163b1747986b03ae863fe43d38d18c242a7
Abdelrahman Ayman <abdelrahman.akefafy@gmail.com>
2024-03-20 23:33:10 +0200

    community model and controller are done (API DOC, update needed)

--- src/controller/create-community.js
+++ src/controller/create-community.js
@@ -1,15 +1,39 @@
-const CreateCommunity = require('./../models/create-community');
+const Community = require('./../models/community');
 const auth = require("../middleware/authentication");
+const router = express.Router();
 
+router.post("/", auth, this.createNewCommunity);
 
 exports.createNewCommunity = auth, async (req, res) => {
 
-    const { name, description, rules } = req.body;
-    //const creatorId = req.user.userName; 
-    //i comment this line fornow untill know how to make it
+    const user = req.user;
+    req.body.creator = user._id;
+    req.body.members = [user._id];
+
+    const { name, description, rules, creator, members } = req.body;
+
+    const createdCommunity = new Community({
+        name: name,
+        description: description,
+        rules: rules,
+        creator: user
+    });
+
     try {
 
+        const savedCommunity = await createdCommunity.save();
+        console.log('Community created successfully');
+
+        return res.status(200).send({ status: 'success' });
+
     } catch (err) {
+        
+        if (err.name == "ValidationError") {
+            res.status(400).send(err.toString());
+        } else {
+            console.error('Error creating the community: ', err);
+            res.status(500).send(err.toString());
+        }
 
     }
 };
--- src/models/community.js
+++ src/models/community.js
@@ -3,23 +3,55 @@ const Schema = mongoose.Schema;
 
 const CommunitySchema = new Schema(
     {
-        communityName: {
+        name: {
             type: String,
-            required: true,
+            unique: [true, "A community with this name already exists"],
+            required: [true, 'You must enter a name for the community'],
+            trim: true,
+            maxlength: 30
         },
-        communityRules: {
+        rules: {
             type: String,
-            required: true,
+            trim: true,
+            maxlength: 200,
+            default: ''
         },
-        communityDiscription: {
+        description: {
             type: String,
-            required: true,
+            trim: true,
+            maxlength: 200,
+            default: ''
+        },
+        creator: {
+            type: Schema.Types.ObjectId,
+            required: [true, "A community must have a creator."],
+            ref: "user"
+        },
+        members: {
+            type: [Schema.Types.ObjectId],
+            required: [true, "A community must have at least one member."],
+            ref: "user", 
+            index: true
         }
 
         //todo add the community settings attributes
         //todo figure out mawdo3 el moderator how is it gonna be saved array or something else?
         //todo mawdo3 el nas ely fi el community how are we gonna save them as well?
-        
-        
+        //todo helper function verfiying that the community 
+
     }
 );
+
+CommunitySchema.statics.checkExistence = async function (name) {
+    const community = await Community.findOne({ name });
+    if (community) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+const Community = mongoose.model("community", UserSchema);
+
+module.exports = Community;
