9caf5cfd607e7e1d0b8f91a3a66d2554cf3ed49d
MohamedMaher03 <m7mdrefaat550@gmail.com>
2024-03-21 01:46:06 +0200

    createCommunity now works

--- src/routes/create-community.js
+++ src/routes/create-community.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../middleware/authentication");
 const createCommunityController = require('../controller/create-community');
 
 router.route('/communities/create', auth)
