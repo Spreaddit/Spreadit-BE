3bedc4e2c101d1fa8f39f742a51a496ec6c27836
Amira El-Garf <elgarfamira4@gmail.com>
2024-03-26 23:28:55 +0200

    forgot-username unit testing is done

--- test/auth.test.js
+++ test/auth.test.js
@@ -225,8 +225,46 @@ test('Test reset-password should return 400 if current password is invalid', asy
     });
 });
 
+test('Test forgot-username should return 404 if user not found', async () => {
 
+    await request(app)
+      .post('/forgot-username')
+      .send({ email: "amiraelgarf99@gmail.com" })
+      .expect(404)
+      .then((response) => {
+        expect(response.body.message).toBe('User not found');
+    });
+});
 
+test('Test forgot-username should return 400 if email does not match username', async () => {
+    
+    const signup = await request(app)
+    .post("/signup")
+    .send({
+        email: "amiraelgarf99@gmail.com",
+        username: "amira12",
+        password: "12345678",
+    });
+    
+    await request(app)
+      .post('/forgot-username')
+      .send({ email: "elgarfamira4@gmail.com" })
+      .expect(404)
+      .then((response) => {
+        expect(response.body.message).toBe("User not found");
+    });
+});
 
-
-
+test('Test forgot-username should send reset password email and return 200 if user found', async () => {
+    const signup = await request(app)
+    .post("/signup")
+    .send({
+        email: "amiraelgarf99@gmail.com",
+        username: "amira12",
+        password: "12345678",
+    });
+    await request(app)
+      .post('/forgot-username')
+      .send({ email: "amiraelgarf99@gmail.com"})
+      .expect(200)
+});
