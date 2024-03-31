8d2e1fb082df25e16f03e9fac6c9146c31473180
Amira El-Garf <elgarfamira4@gmail.com>
2024-03-31 14:01:15 +0200

    file upload locally is set successfully

--- src/configuration.js
+++ src/configuration.js
@@ -3,3 +3,5 @@ exports.cloudConnectString = "mongodb+srv://mahmoudaly964:mahmoud@spreddit.7wsff
 exports.testConnectionString = "mongodb://localhost:27017/testDB";
 exports.googleClientSecret = "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";
 exports.googleClientID = "117650718501-b2o690ebq8fun5517rpogqc2tvtl2nq6.apps.googleusercontent.com";
+
+exports.uploadPath = "./data/";
