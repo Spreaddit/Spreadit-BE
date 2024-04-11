698ba58c0340bc12abed359af73f7ef5ec006667
Amira El-Garf <elgarfamira4@gmail.com>
2024-04-12 01:01:02 +0200

    data seeding modification

---
+++ seed-data/rule.js
@@ -0,0 +1,19 @@
+const ruleSeedData = [
+    {
+      _id: "624a6598ce588b8cd1e84f6f",
+      title: "Be Respectful",
+      description: "Treat others with respect and kindness.",
+      reportReason: "Disrespectful behavior",
+      communityName: "Programming",
+    },
+    {
+      _id: "624a6956a807a4016790d545",
+      title: "No Hate Speech",
+      description: "Do not engage in hate speech or discrimination.",
+      reportReason: "Hate speech",
+      communityName: "Gaming", 
+    },
+];
+  
+exports.data = ruleSeedData;
+  
