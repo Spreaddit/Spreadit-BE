a43a5d5f5af84a261e326d3a6b7812fff62c9ed8
MohamedMaher03 <m7mdrefaat550@gmail.com>
2024-04-13 15:48:09 +0200

    update auth in mobile settings

--- src/routes/mobile-settings.js
+++ src/routes/mobile-settings.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const mobileSettingsController = require('../controller/mobile-settings');
 const auth = require("../middleware/authentication");
 
-router.route('/general/account', auth)
-    .get(mobileSettingsController.getAccountSettings)
-    .put(mobileSettingsController.modifyAccountSettings)
-    .delete(mobileSettingsController.deleteAccount);
+router.route('/general/account')
+    .get(auth.authentication, mobileSettingsController.getAccountSettings)
+    .put(auth.authentication, mobileSettingsController.modifyAccountSettings)
+    .delete(auth.authentication, mobileSettingsController.deleteAccount);
 
-router.route('/blocking-permissions', auth)
-    .get(mobileSettingsController.getBlockingSetting)
-    .put(mobileSettingsController.modifyBlockingSetting);
+router.route('/blocking-permissions')
+    .get(auth.authentication, mobileSettingsController.getBlockingSetting)
+    .put(auth.authentication, mobileSettingsController.modifyBlockingSetting);
 
-router.route('/contact', auth)
-    .get(mobileSettingsController.getContactSetting)
-    .put(mobileSettingsController.modifyContactSetting);
+router.route('/contact')
+    .get(auth.authentication, mobileSettingsController.getContactSetting)
+    .put(auth.authentication, mobileSettingsController.modifyContactSetting);
 
 module.exports = router;
