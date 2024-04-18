5aca1e18d6b2e1b68f02b656df3c2ecfef550228
Amira El-Garf <elgarfamira4@gmail.com>
2024-04-18 21:59:43 +0200

    VAPID-key generation API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,3 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./../models/user");
+const Post = require("./../models/post");
+const Comment = require("./../models/comment");
+
+
+//when the user changes his username it should be updated in the posts as well 
+
+router.get("/vapid-key", auth, async (req, res) => {
+    try {
+      const vapidKeys = webPush.generateVAPIDKeys();
+      if (vapidKeys) {
+        return res.status(200).send({
+          publicKey: vapidKeys.publicKey,
+          privateKey: vapidKeys.privateKey,
+        });
+      } else {
+        return res.status(500).send({
+          message: "Vapid keys could not be generated",
+        });
+      }
+    } catch (error) {
+      res.status(500).send(error.toString());
+    }
+});
