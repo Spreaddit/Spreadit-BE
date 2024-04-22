12c319119ba05bae29bb01131143882cc8cca4bf
mahmoudaly964 <mahmoudaly964@gmail.com>
2024-04-22 21:32:51 +0200

    make delete recent posts and some apis in messages

--- src/routes/message.js
+++ src/routes/message.js
@@ -11,6 +11,18 @@ router
   .get(auth.authentication, messageController.getInboxMessages);
 
 router
+  .route("/message/messages/")
+  .get(auth.authentication, messageController.getAllMessages);
+
+router
+  .route("/message/postreplies/")
+  .get(auth.authentication, messageController.getPostReplies);
+
+router
+  .route("/message/sent/")
+  .get(auth.authentication, messageController.getSentMessages);
+
+router
   .route("/message/deletemsg/:messageId")
   .delete(auth.authentication, messageController.deleteMessage);
 
--- src/routes/post.js
+++ src/routes/post.js
@@ -4,82 +4,98 @@ const postController = require("../controller/post-controller");
 const upload = require("../service/fileUpload");
 const auth = require("../middleware/authentication");
 
+router
+  .route("/")
+  .post(
+    auth.authentication,
+    upload.array("attachments"),
+    postController.createPost
+  );
 
-router.route('/')
-    .post(auth.authentication, upload.array('attachments'), postController.createPost);
+router.route("/:postId").delete(auth.authentication, postController.deletePost);
 
+router
+  .route("/community/:community")
+  .get(auth.authentication, postController.getAllPostsInCommunity);
 
+router.route("/save").get(auth.authentication, postController.getSavedPosts);
 
-router.route("/:postId")
-    .delete(auth.authentication, postController.deletePost);
+router
+  .route("/:postId/save")
+  .post(auth.authentication, postController.savePost);
 
-router.route('/community/:community')
-    .get(auth.authentication, postController.getAllPostsInCommunity)
+router
+  .route("/:postId/unsave")
+  .post(auth.authentication, postController.unsavePost);
 
-router.route("/save")
-    .get(auth.authentication, postController.getSavedPosts);
+router.route("/:postId/edit").put(auth.authentication, postController.editPost);
 
+router
+  .route("/:postId/spoiler")
+  .post(auth.authentication, postController.spoilerPostContent);
 
+router
+  .route("/:postId/unspoiler")
+  .post(auth.authentication, postController.unspoilerPostContent);
 
-router.route('/:postId/save')
-    .post(auth.authentication, postController.savePost);
+router
+  .route("/:postId/lock")
+  .post(auth.authentication, postController.lockPostComments);
 
-router.route('/:postId/unsave')
-    .post(auth.authentication, postController.unsavePost);
+router
+  .route("/:postId/unlock")
+  .post(auth.authentication, postController.unlockPostComments);
 
-router.route('/:postId/edit')
-    .put(auth.authentication, postController.editPost);
+router
+  .route("/:postId/upvote")
+  .post(auth.authentication, postController.upvotePost);
 
-router.route('/:postId/spoiler')
-    .post(auth.authentication, postController.spoilerPostContent);
+router
+  .route("/:postId/downvote")
+  .post(auth.authentication, postController.downvotePost);
 
-router.route('/:postId/unspoiler')
-    .post(auth.authentication, postController.unspoilerPostContent);
+router
+  .route("/downvote")
+  .get(auth.authentication, postController.getDownvotedPosts);
 
-router.route('/:postId/lock')
-    .post(auth.authentication, postController.lockPostComments);
+router
+  .route("/upvote")
+  .get(auth.authentication, postController.getUpvotedPosts);
 
-router.route('/:postId/unlock')
-    .post(auth.authentication, postController.unlockPostComments);
+router
+  .route("/:postId/hide")
+  .post(auth.authentication, postController.hidePost);
 
-router.route('/:postId/upvote')
-    .post(auth.authentication, postController.upvotePost);
+router
+  .route("/:postId/unhide")
+  .post(auth.authentication, postController.unhidePost);
 
-router.route('/:postId/downvote')
-    .post(auth.authentication, postController.downvotePost);
+router.route("/hide").get(auth.authentication, postController.getHiddenPosts);
 
-router.route('/downvote')
-    .get(auth.authentication, postController.getDownvotedPosts);
+router
+  .route("/:postId/nsfw")
+  .post(auth.authentication, postController.markPostAsNsfw);
 
-router.route('/upvote')
-    .get(auth.authentication, postController.getUpvotedPosts);
+router
+  .route("/:postId/unnsfw")
+  .post(auth.authentication, postController.markPostAsNotNsfw);
 
-router.route('/:postId/hide')
-    .post(auth.authentication, postController.hidePost);
+router
+  .route("/:postId/report")
+  .post(auth.authentication, postController.reportPost);
 
-router.route('/:postId/unhide')
-    .post(auth.authentication, postController.unhidePost);
+router
+  .route("/:postId/poll/vote")
+  .post(auth.authentication, postController.voteInPoll);
 
-router.route("/hide")
-    .get(auth.authentication, postController.getHiddenPosts);
+router
+  .route("/username/:username")
+  .get(auth.authentication, postController.getAllUserPosts);
 
-router.route('/:postId/nsfw')
-    .post(auth.authentication, postController.markPostAsNsfw);
+router.route("/:postId").get(auth.authentication, postController.getPostById);
 
-router.route('/:postId/unnsfw')
-    .post(auth.authentication, postController.markPostAsNotNsfw);
-
-router.route('/:postId/report')
-    .post(auth.authentication, postController.reportPost);
-
-router.route('/:postId/poll/vote')
-    .post(auth.authentication, postController.voteInPoll);
-
-
-router.route('/username/:username')
-    .get(auth.authentication, postController.getAllUserPosts)
-
-router.route("/:postId")
-    .get(auth.authentication, postController.getPostById);
+router
+  .route("/deleterecent/:postId")
+  .delete(auth.authentication, postController.deleteRecentPost);
 
 module.exports = router;
