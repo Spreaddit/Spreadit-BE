51a4f568429b32f6fa29caab4cd780ccec452073
mahmoudaly964 <mahmoudaly964@gmail.com>
2024-05-01 01:20:51 +0300

    make unblock user and get message by id

--- src/controller/block-user.js
+++ src/controller/block-user.js
@@ -45,3 +45,42 @@ exports.blockUser = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+exports.unBlock = async (req, res) => {
+  try {
+    const username = req.body.username;
+    if (!username) {
+      return res.status(400).json({ error: "Username is required" });
+    }
+    const user = await BlockUser.getUserByEmailOrUsername(username);
+    if (!user) {
+      console.error("User not found");
+      return res.status(404).json({ error: "User not found" });
+    }
+    const toUnblockId = user._id;
+
+    const unBlockerId = req.user._id;
+
+    if (toUnblockId.equals(unBlockerId)) {
+      console.log("user cannot unblock himself");
+      return res.status(400).json({ error: "user cannot unblock himself" });
+    }
+    if (!toUnblockId || !unBlockerId) {
+      console.error("this user not found:");
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const tounblockerUser = await BlockUser.findByIdAndUpdate(
+      unBlockerId,
+      { $pull: { blockedUsers: toUnblockId } },
+      { new: true }
+    );
+    const response = {
+      description: "User unBlocked successfully",
+    };
+    res.status(200).json(response);
+  } catch (err) {
+    console.error("Error unBlocked user", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
