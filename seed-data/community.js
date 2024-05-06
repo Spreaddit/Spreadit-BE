175f9100101e07f78e1e5d9724af0f6d1074e0ee
Farouq Diaa <farouqdiaaeldin@gmail.com>
2024-05-06 20:14:50 +0300

    finished function documentation

--- seed-data/community.js
+++ seed-data/community.js
@@ -3,7 +3,7 @@ const communitySeedData = [
     _id: "624a6a677c8d9c9f5fd5eb1a",
     name: "CodeCrafters",
     category: "Technology",
-    rules: ["624a6a677c8d9c9f5fd5eb2b"], 
+    rules: ["624a6a677c8d9c9f5fd5eb2b"],
     description: "A community for coding enthusiasts to share knowledge, projects, and programming tips.",
     is18plus: false,
     allowNfsw: true,
@@ -13,8 +13,8 @@ const communitySeedData = [
     members: ["624a4a94c66738f13854b474", "624a4fbf3f392aefdb4dd1c8", "624a6a677c8d9c9f5fd5eb0b"], // Amira, Farouq, Abdullah
     moderators: ["624a4a94c66738f13854b474"], // Amira
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb1b",
     name: "TechTalks",
     category: "Technology",
@@ -28,12 +28,12 @@ const communitySeedData = [
     members: ["624a52d75ff69df002d25035", "624a4fbf3f392aefdb4dd1c8", "624a6a677c8d9c9f5fd5eb0e"], // Mahmoud, Farouq, Basma
     moderators: ["624a52d75ff69df002d25035"], // Mahmoud
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb1c",
     name: "CodeGeeks",
     category: "Technology",
-    rules: ["624a6a677c8d9c9f5fd5eb2d"], 
+    rules: ["624a6a677c8d9c9f5fd5eb2d"],
     description: "Connect with fellow coders, collaborate on projects, and solve coding challenges together.",
     is18plus: false,
     allowNfsw: true,
@@ -43,12 +43,12 @@ const communitySeedData = [
     members: ["624a52d75ff69df002d25035", "624a6a677c8d9c9f5fd5eb0e", "624a6a677c8d9c9f5fd5eb0b"], // Mahmoud, Basma, Abdullah
     moderators: ["624a52d75ff69df002d25035"], // Mahmoud
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a48f143bd949036986953",
     name: "ScienceExplorersClub",
     category: "Science",
-    rules: ["624a6a677c8d9c9f5fd5eb2e"], 
+    rules: ["624a6a677c8d9c9f5fd5eb2e"],
     description: "Dive into the fascinating world of science with discussions on physics, chemistry, and more.",
     is18plus: false,
     allowNfsw: true,
@@ -58,13 +58,14 @@ const communitySeedData = [
     members: ["624a6a677c8d9c9f5fd5eb16", "624a6a677c8d9c9f5fd5eb0f", "624a6a677c8d9c9f5fd5eb14"], // Mahmoud, Farida, Karim
     moderators: ["624a6a677c8d9c9f5fd5eb16"], // Mahmoud
     membersCount: 3,
-},
-{ 
+  },
+  {
     _id: "624a4052e28fb9d9c024671a",
     name: "SpaceAndAstronomyLovers",
     category: "Science",
-    rules: ["624a6a677c8d9c9f5fd5eb2f"], 
-    description: "A community for astronomy enthusiasts to share celestial observations, news, and theories about the cosmos.",
+    rules: ["624a6a677c8d9c9f5fd5eb2f"],
+    description:
+      "A community for astronomy enthusiasts to share celestial observations, news, and theories about the cosmos.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -73,13 +74,14 @@ const communitySeedData = [
     members: ["624a6a677c8d9c9f5fd5eb13", "624a6a677c8d9c9f5fd5eb14", "624a6a677c8d9c9f5fd5eb11"], // Galal, Karim, Rehab
     moderators: ["624a6a677c8d9c9f5fd5eb13"], // Galal
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb1d",
     name: "BiologyBuffsSociety",
     category: "Science",
-    rules: ["624a6a677c8d9c9f5fd5eb30"], 
-    description: "Delve into the intricate world of biology with discussions on genetics, ecology, and evolutionary biology.",
+    rules: ["624a6a677c8d9c9f5fd5eb30"],
+    description:
+      "Delve into the intricate world of biology with discussions on genetics, ecology, and evolutionary biology.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -89,13 +91,14 @@ const communitySeedData = [
     contributors: ["624a6a677c8d9c9f5fd5eb0e", "624a6a677c8d9c9f5fd5eb0f", "624a6a677c8d9c9f5fd5eb12"], // Basma, Farida, Youssef
     moderators: ["624a6a677c8d9c9f5fd5eb0e"], // Basma
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb1e",
     name: "CreativeMindsCollective",
     category: "Art and Creativity",
     rules: ["624a6a677c8d9c9f5fd5eb31"],
-    description: "Share your artistic creations, whether it's paintings, photography, music, or poetry, and engage with fellow creatives.",
+    description:
+      "Share your artistic creations, whether it's paintings, photography, music, or poetry, and engage with fellow creatives.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -106,12 +109,12 @@ const communitySeedData = [
     moderators: ["624a4fbf3f392aefdb4dd1c8"], // Farouq
     membersCount: 3,
     removalReasons: ["6633ddb655d6bba0f4023c40"],
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb1f",
     name: "VisualArtsGallery",
     category: "Art and Creativity",
-    rules: ["624a6a677c8d9c9f5fd5eb32"], 
+    rules: ["624a6a677c8d9c9f5fd5eb32"],
     description: "Showcase and discuss visual arts, including drawings, paintings, sculptures, and digital art.",
     is18plus: false,
     allowNfsw: true,
@@ -121,13 +124,14 @@ const communitySeedData = [
     members: ["624a6a677c8d9c9f5fd5eb0e", "624a6a677c8d9c9f5fd5eb14", "624a4fbf3f392aefdb4dd1c8"], // Basma, Karim, Farouq
     moderators: ["624a6a677c8d9c9f5fd5eb0e"], // Basma
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb20",
     name: "Writers'Haven",
     category: "Art and Creativity",
-    rules: ["624a6a677c8d9c9f5fd5eb33"], 
-    description: "A haven for writers to share their prose, poetry, and stories, and receive feedback and support from fellow writers.",
+    rules: ["624a6a677c8d9c9f5fd5eb33"],
+    description:
+      "A haven for writers to share their prose, poetry, and stories, and receive feedback and support from fellow writers.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -136,13 +140,14 @@ const communitySeedData = [
     members: ["624a4a94c66738f13854b474", "624a6a677c8d9c9f5fd5eb0e", "624a6a677c8d9c9f5fd5eb0f"], // Amira, Basma, Farida
     moderators: ["624a4a94c66738f13854b474"], // Amira
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb21",
     name: "MindfulnessAndMeditation",
     category: "Mental Health and Psychology",
-    rules: ["624a6a677c8d9c9f5fd5eb34"], 
-    description: "Explore mindfulness practices, meditation techniques, and mental well-being strategies in a supportive community environment.",
+    rules: ["624a6a677c8d9c9f5fd5eb34"],
+    description:
+      "Explore mindfulness practices, meditation techniques, and mental well-being strategies in a supportive community environment.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -151,13 +156,14 @@ const communitySeedData = [
     members: ["624a6a677c8d9c9f5fd5eb0c", "624a6a677c8d9c9f5fd5eb0d", "624a6a677c8d9c9f5fd5eb0f"], // Mimo, Basem, Farida
     moderators: ["624a6a677c8d9c9f5fd5eb0c"], // Mimo
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb22",
     name: "MentalHealthSupportGroup",
     category: "Mental Health and Psychology",
     rules: ["624a6a677c8d9c9f5fd5eb35"],
-    description: "A safe space for individuals to discuss mental health challenges, seek support, and share coping mechanisms.",
+    description:
+      "A safe space for individuals to discuss mental health challenges, seek support, and share coping mechanisms.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -166,13 +172,14 @@ const communitySeedData = [
     members: ["624a4a94c66738f13854b474", "624a6a677c8d9c9f5fd5eb0f", "624a6a677c8d9c9f5fd5eb0d"], // Amira, Farida, Basem
     moderators: ["624a4a94c66738f13854b474"], // Amira
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb36",
     name: "PsychologyInsightsCircle",
     category: "Mental Health and Psychology",
-    rules: ["624a6a677c8d9c9f5fd5eb3e"], 
-    description: "Delve into the fascinating field of psychology with discussions on human behavior, cognition, and emotions.",
+    rules: ["624a6a677c8d9c9f5fd5eb3e"],
+    description:
+      "Delve into the fascinating field of psychology with discussions on human behavior, cognition, and emotions.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -181,13 +188,14 @@ const communitySeedData = [
     members: ["624a6a677c8d9c9f5fd5eb16", "624a6a677c8d9c9f5fd5eb0e", "624a6a677c8d9c9f5fd5eb14"], // Mahmoud, Basma, Karim
     moderators: ["624a6a677c8d9c9f5fd5eb16"], // Mahmoud
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb24",
     name: "GlobetrottersCommunity",
     category: "Travel and Adventure",
     rules: ["624a6a677c8d9c9f5fd5eb37"],
-    description: "Connect with fellow travelers, share travel experiences, tips, and recommendations, and inspire each other to explore the world.",
+    description:
+      "Connect with fellow travelers, share travel experiences, tips, and recommendations, and inspire each other to explore the world.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -196,13 +204,14 @@ const communitySeedData = [
     members: ["624a4fbf3f392aefdb4dd1c8", "624a6a677c8d9c9f5fd5eb0e", "624a6a677c8d9c9f5fd5eb0f"], // Farouq, Basma, Farida
     moderators: ["624a4fbf3f392aefdb4dd1c8"], // Farouq
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb25",
     name: "AdventurousSoulsSociety",
     category: "Travel and Adventure",
     rules: ["624a6a677c8d9c9f5fd5eb38"],
-    description: "Embark on exciting adventures, share thrilling travel stories, and exchange tips for off-the-beaten-path destinations.",
+    description:
+      "Embark on exciting adventures, share thrilling travel stories, and exchange tips for off-the-beaten-path destinations.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -211,13 +220,14 @@ const communitySeedData = [
     members: ["624a6a677c8d9c9f5fd5eb0b", "624a6a677c8d9c9f5fd5eb16", "624a6a677c8d9c9f5fd5eb0c"], // Abdullah, Mahmoud, Mimo
     moderators: ["624a6a677c8d9c9f5fd5eb0b"], // Abdullah
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb26",
     name: "CulinaryDelights",
     category: "Food and Cooking",
-    rules: ["624a6a677c8d9c9f5fd5eb39"], 
-    description: "Indulge in discussions about mouth-watering recipes, cooking techniques, and culinary adventures from around the globe.",
+    rules: ["624a6a677c8d9c9f5fd5eb39"],
+    description:
+      "Indulge in discussions about mouth-watering recipes, cooking techniques, and culinary adventures from around the globe.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -226,13 +236,14 @@ const communitySeedData = [
     members: ["624a4fbf3f392aefdb4dd1c8", "624a6a677c8d9c9f5fd5eb0e", "624a6a677c8d9c9f5fd5eb0c"], // Farouq, Basma, Mimo
     moderators: ["624a4fbf3f392aefdb4dd1c8"], // Farouq
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb27",
     name: "GourmetAdventures",
     category: "Food and Cooking",
-    rules: ["624a6a677c8d9c9f5fd5eb3a"], 
-    description: "Explore the world of gourmet cuisine, share dining experiences, and discover hidden culinary gems in your city.",
+    rules: ["624a6a677c8d9c9f5fd5eb3a"],
+    description:
+      "Explore the world of gourmet cuisine, share dining experiences, and discover hidden culinary gems in your city.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -241,13 +252,14 @@ const communitySeedData = [
     members: ["624a6a677c8d9c9f5fd5eb0d", "624a6a677c8d9c9f5fd5eb0b", "624a6a677c8d9c9f5fd5eb13"], // Basem, Abdullah, Galal
     moderators: ["624a6a677c8d9c9f5fd5eb0d"], // Basem
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb28",
     name: "PopCultureFanatics",
     category: "Entertainment and Pop Culture",
-    rules: ["624a6a677c8d9c9f5fd5eb3b"], 
-    description: "Celebrate all things entertainment, from movies and TV shows to music, celebrities, and internet memes.",
+    rules: ["624a6a677c8d9c9f5fd5eb3b"],
+    description:
+      "Celebrate all things entertainment, from movies and TV shows to music, celebrities, and internet memes.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -256,13 +268,14 @@ const communitySeedData = [
     members: ["624a6a677c8d9c9f5fd5eb16", "624a6a677c8d9c9f5fd5eb0e", "624a6a677c8d9c9f5fd5eb0c"], // Mahmoud, Basma, Mimo
     moderators: ["624a6a677c8d9c9f5fd5eb16"], // Mahmoud
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb29",
     name: "MovieBuffsClub",
     category: "Entertainment and Pop Culture",
-    rules: ["624a6a677c8d9c9f5fd5eb3c"], 
-    description: "Discuss the latest movie releases, share reviews, recommendations, and indulge in lively debates about classic films.",
+    rules: ["624a6a677c8d9c9f5fd5eb3c"],
+    description:
+      "Discuss the latest movie releases, share reviews, recommendations, and indulge in lively debates about classic films.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -271,13 +284,14 @@ const communitySeedData = [
     members: ["624a4a94c66738f13854b474", "624a6a677c8d9c9f5fd5eb0d", "624a6a677c8d9c9f5fd5eb13"], // Amira, Basem, Galal
     moderators: ["624a4a94c66738f13854b474"], // Amira
     membersCount: 3,
-},
-{
+  },
+  {
     _id: "624a6a677c8d9c9f5fd5eb2a",
     name: "MusicManiacsLounge",
     category: "Entertainment and Pop Culture",
     rules: ["624a6a677c8d9c9f5fd5eb3d"],
-    description: "Dive into the world of music, share your favorite songs, artists, genres, and concert experiences with fellow music enthusiasts.",
+    description:
+      "Dive into the world of music, share your favorite songs, artists, genres, and concert experiences with fellow music enthusiasts.",
     is18plus: false,
     allowNfsw: true,
     allowSpoile: true,
@@ -286,10 +300,50 @@ const communitySeedData = [
     members: ["624a6a677c8d9c9f5fd5eb0c", "624a6a677c8d9c9f5fd5eb16", "624a6a677c8d9c9f5fd5eb13"], // Mimo, Mahmoud, Galal
     moderators: ["624a6a677c8d9c9f5fd5eb0c"], // Mimo
     membersCount: 3,
-},
-
-    
+  },
 ];
-  
+
+const generateRandomInsights = () => {
+  const randomViews = Math.floor(Math.random() * 1000);
+  const randomNewMembers = Math.floor(Math.random() * 50);
+  const randomLeavingMembers = Math.floor(Math.random() * 20);
+  return {
+    views: randomViews,
+    newMembers: randomNewMembers,
+    leavingMembers: randomLeavingMembers,
+  };
+};
+
+communitySeedData.forEach((community) => {
+  const today = new Date();
+  const lastSevenDays = new Date(today);
+  lastSevenDays.setDate(lastSevenDays.getDate() - 6);
+
+  const lastThirtyMonths = new Date(today);
+  lastThirtyMonths.setMonth(lastThirtyMonths.getMonth() - 29);
+
+  const randomInsightsLast7Days = [];
+  const randomInsightsLast30Months = [];
+
+  for (let i = 0; i < 7; i++) {
+    const date = new Date(lastSevenDays);
+    date.setDate(lastSevenDays.getDate() + i);
+    randomInsightsLast7Days.push({
+      month: date,
+      ...generateRandomInsights(),
+    });
+  }
+
+  for (let i = 0; i < 30; i++) {
+    const date = new Date(lastThirtyMonths);
+    date.setMonth(lastThirtyMonths.getMonth() + i);
+    randomInsightsLast30Months.push({
+      month: date,
+      ...generateRandomInsights(),
+    });
+  }
+
+  community.last7DaysInsights = randomInsightsLast7Days;
+  community.monthlyInsights = randomInsightsLast30Months;
+});
 exports.data = communitySeedData;
-  
