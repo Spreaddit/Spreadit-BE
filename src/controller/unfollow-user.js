66fa14b1938e12140cc28d52c8275ad63ffbcbf5
mahmoudaly964 <mahmoudaly964@gmail.com>
2024-05-07 22:49:21 +0300

    make useraction test

--- src/controller/unfollow-user.js
+++ src/controller/unfollow-user.js
@@ -9,7 +9,6 @@ exports.unfollowUser = async (req, res) => {
     }
     const user = await UnfollowUser.getUserByEmailOrUsername(username);
     if (!user) {
-      console.error("User not found");
       return res.status(404).json({ error: "User not found" });
     }
     const toUnfollowID = user._id;
@@ -17,11 +16,9 @@ exports.unfollowUser = async (req, res) => {
     const unfollowerID = req.user._id;
 
     if (toUnfollowID.equals(unfollowerID)) {
-      console.log("user cannot unfollow himself");
       return res.status(400).json({ error: "user cannot unfollow himself" });
     }
     if (!toUnfollowID || !unfollowerID) {
-      console.error("this user not found:");
       return res.status(404).json({ error: "User not found" });
     }
 
@@ -41,7 +38,6 @@ exports.unfollowUser = async (req, res) => {
     };
     res.status(200).json(response);
   } catch (err) {
-    console.error("Error unfollow user", err);
     res.status(500).json({ error: "Internal server error" });
   }
 };
