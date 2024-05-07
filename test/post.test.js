bc2af81e80dd2a3ce4466cdc4fb4c9d7be553951
MohamedMaher03 <m7mdrefaat550@gmail.com>
2024-05-07 20:46:23 +0300

    finish unit test of settings

--- test/post.test.js
+++ test/post.test.js
@@ -40,52 +40,6 @@ afterAll(() => {
   mongoose.connection.close();
 });
 
-describe("POST /api/posts", () => {
-  test("It should create a new post", async () => {
-    const loginResponse = await request(app)
-      .post("/login")
-      .send({ username: "maher", password: "myPassw@ord123" });
-
-    const tokenlogin = loginResponse.body.access_token;
-
-    const cresponse = await request(app)
-      .post("/community/create")
-      .set("Authorization", `Bearer ${tokenlogin}`)
-      .send({
-        name: "Test Community66",
-        is18plus: true,
-        communityType: "Public",
-      })
-      .expect(200);
-
-    const createdCommunity = await Community.findOne({
-      name: "Test Community66",
-    });
-
-    const communityId = createdCommunity._id;
-
-    const user = await User.findOne({ username: "maher" });
-
-    user.subscribedCommunities.push(communityId);
-    await user.save();
-
-    const newPost = {
-      title: "Test Post",
-      content: "This is a test post.",
-      community: "Test Community66",
-      type: "Post",
-    };
-
-    const response = await request(app)
-      .post("/")
-      .set("Authorization", `Bearer ${tokenlogin}`)
-      .send(newPost)
-      .expect(201)
-      .then((response) => {
-        expect(response.body.message).toBe("Post created successfully");
-      });
-  });
-});
 
 describe("POST /api/posts", () => {
   test("It should create a new post", async () => {
@@ -133,7 +87,7 @@ describe("GET /api/posts", () => {
 describe("GET /api/posts/:id", () => {
   test("It should return 404 if post is not found", async () => {
     await request(app)
-      .get("/api/posts/123456789012345678901234") 
+      .get("/api/posts/123456789012345678901234")
       .expect(404);
   });
 });
@@ -141,7 +95,7 @@ describe("GET /api/posts/:id", () => {
 describe("PUT /api/posts/:id", () => {
   test("It should return 404 if post is not found", async () => {
     await request(app)
-      .put("/api/posts/123456789012345678901234") 
+      .put("/api/posts/123456789012345678901234")
       .expect(404);
   });
 });
@@ -149,7 +103,7 @@ describe("PUT /api/posts/:id", () => {
 describe("DELETE /api/posts/:id", () => {
   test("It should return 404 if post is not found", async () => {
     await request(app)
-      .delete("/api/posts/123456789012345678901234") 
+      .delete("/api/posts/123456789012345678901234")
       .expect(404);
   });
 });
@@ -569,56 +523,6 @@ describe("report post", () => {
   });
 });
 
-/* describe("vote post", () => {
-    test("It should vote  post", async () => {
-         const loginResponse = await request(app)
-            .post("/login")
-            .send({ username: "maher", password: "myPassw@ord123" });
-
-        const tokenlogin = loginResponse.body.access_token;
-
-        const cresponse = await request(app)
-            .post("/community/create")
-            .set("Authorization", `Bearer ${tokenlogin}`)
-            .send({
-                name: "Test Community66",
-                is18plus: true,
-                communityType: "Public",
-            })
-            .expect(200);
-
-        const createdCommunity = await Community.findOne({ name: "Test Community66" });
-
-        const communityId = createdCommunity._id;
-
-        const user = await User.findOne({ username: "maher" });
-
-        user.subscribedCommunities.push(communityId);
-        await user.save();
-
-        const newPost = {
-            title: "Test Post",
-            content: "This is a test post.",
-            community: "Test Community66",
-            type: "Post",
-        };
-        const response2 = await request(app)
-            .post("/")
-            .set("Authorization", `Bearer ${tokenlogin}`)
-            .send(newPost)
-            .expect(201);
-        const postId = response2.body.postId;
-
-
-        const response3 = await request(app)
-            .post(`/${postId}/poll/vote`)
-            .send({ selectedOption: "amira12amira" })
-            .set("Authorization", `Bearer ${tokenlogin}`)
-            .expect(200);
-
-    });
-
-}); */
 
 describe("hide post", () => {
   test("It should hide  post", async () => {
@@ -719,7 +623,7 @@ describe("unhide post", () => {
     const response3 = await request(app)
       .post(`/${postId}/unhide`)
       .set("Authorization", `Bearer ${tokenlogin}`)
-      .expect(200); 
+      .expect(200);
   });
 });
 
@@ -768,7 +672,7 @@ describe("unhide post", () => {
     const response3 = await request(app)
       .post(`/${postId}/unhide`)
       .set("Authorization", `Bearer ${tokenlogin}`)
-      .expect(400); 
+      .expect(400);
   });
 });
 
