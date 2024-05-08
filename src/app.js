4a6ec2498af87899458cdf1bada385ce97ecdf5f
Amira El-Garf <elgarfamira4@gmail.com>
2024-05-08 20:42:19 +0300

    devops port adjustment

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ const MessageSeeder = require("../seeders/message.seeder");
 const ConversationSeeder = require("../seeders/conversation.seeder");
 
 const app = express();
-const port = 80;
+const port = 5300;
 const connectionurl = config.cloudConnectString;
 
 app.use(express.json());
